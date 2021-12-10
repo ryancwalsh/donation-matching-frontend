d6c59e0951473b39a70e92214aa8e976c675b1aa
Ryan Walsh <rcwalsh@gmail.com>
2021-12-10 14:01:47 -0500

    explanation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,8 @@ function IndexPage() {
   return (
     <Layout>
       <Seo title="Home" />
+      <p>This page is just a barebones demo to prove that the underlying smart contract described at <a href="https://github.com/ryancwalsh/donation-matching">https://github.com/ryancwalsh/donation-matching</a> works.</p>
+      <p>Please read the overview there and forgive the lack of guidance here.</p>
       <h1>Choose a recipient:</h1>
       <form action="recipient">
         <input type="text" name="recipient" placeholder={`AccountId, such as ${sampleRecipient}`} style={{ width: '100%' }} />
